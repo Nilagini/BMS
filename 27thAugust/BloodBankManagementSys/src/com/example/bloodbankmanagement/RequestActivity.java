dfaa5b406de4c9a5f456a41f4fa15321af387769
Nilasini <nilathiru93@gmail.com>
2015-08-27 19:29:47 +0530

    adding a new folder

---
+++ 27thAugust/BloodBankManagementSys/src/com/example/bloodbankmanagement/RequestActivity.java
@@ -0,0 +1,126 @@
+package com.example.bloodbankmanagement;
+
+import com.example.bloodbankmanagement.ThirdActivity;
+
+import android.R.integer;
+import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bloodbankmanagement.R;
+
+public class RequestActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.request_page);
+
+		  Button btn2=(Button) findViewById(R.id.button2);
+		  btn2.setOnClickListener(new OnClickListener( ) { public void
+		  onClick(View v){ Intent intent=new
+		  Intent(v.getContext(),SearchActivity.class);
+		  startActivityForResult(intent, 0); } });
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void showdetail(View view) {
+		ThirdActivity th = new ThirdActivity();
+		try {
+			SQLiteDatabase mydbnw = openOrCreateDatabase(th.DBNAME,
+					Context.MODE_PRIVATE, null);
+			Cursor c = mydbnw.rawQuery("SELECT * from " + th.TABLE, null);
+			int count = c.getCount();
+			c.moveToFirst();
+			TableLayout tableLayout = new TableLayout(getApplicationContext());
+			tableLayout.setVerticalScrollBarEnabled(true);
+			TableRow tableRow, tableRow1, tableRow2 = null;
+			TextView textView, textView1, textView2, textView3;
+			tableRow = new TableRow(getApplicationContext());
+			tableRow1 = new TableRow(getApplicationContext());
+			tableRow2 = new TableRow(getApplicationContext());
+			textView = new TextView(getApplicationContext());
+			textView1 = new TextView(getApplicationContext());
+			textView2 = new TextView(getApplicationContext());
+
+			textView.setText("Blood_group");
+			textView.setTextColor(Color.RED);
+			textView.setTypeface(null, Typeface.BOLD);
+			textView.setPadding(10, 10, 10, 10);
+
+			tableRow.addView(textView);
+			tableLayout.addView(tableRow);
+
+			textView1.setText("Location");
+			textView1.setTextColor(Color.RED);
+			textView1.setTypeface(null, Typeface.BOLD);
+			textView1.setPadding(10, 10, 10, 10);
+
+			tableRow.addView(textView1);
+			tableLayout.addView(tableRow1);
+
+			textView2.setText("Available");
+			textView2.setTextColor(Color.RED);
+			textView2.setTypeface(null, Typeface.BOLD);
+			textView2.setPadding(10, 10, 10, 10);
+
+			tableRow.addView(textView2);
+			tableLayout.addView(tableRow2);
+
+			for (Integer j = 0; j < count; j++) {
+				tableRow = new TableRow(getApplicationContext());
+
+				textView2 = new TextView(getApplicationContext());
+				textView2.setText(c.getString(c.getColumnIndex("BLOOD_GROUP")));
+				textView2.setPadding(10, 10, 10, 10);
+				tableRow.addView(textView2);
+
+				tableRow1 = new TableRow(getApplicationContext());
+				textView1 = new TextView(getApplicationContext());
+				textView1.setText(c.getString(c.getColumnIndex("LOCATION")));
+				textView1.setPadding(10, 10, 10, 10);
+				tableRow.addView(textView1);
+
+				textView3 = new TextView(getApplicationContext());
+				textView3.setText(c.getString(c.getColumnIndex("AVAILABLE")));
+				textView3.setPadding(10, 10, 10, 10);
+				tableRow.addView(textView3);
+
+				tableLayout.addView(tableRow);
+				c.moveToNext();
+
+			}
+			setContentView(tableLayout);
+			mydbnw.close();
+		} catch (Exception e) {
+			Toast.makeText(getApplicationContext(), "Error encountered.",
+					Toast.LENGTH_LONG);
+		}
+
+	}
+
+}
---
+++ 27thAugust/BloodBankManagementSys/src/com/example/bloodbankmanagement/SearchActivity.java
@@ -0,0 +1,222 @@
+package com.example.bloodbankmanagement;
+import com.example.bloodbankmanagement.ThirdActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SearchActivity extends ActionBarActivity {
+	private String location, bloodgroup;
+	String dis;
+	String ur1;
+	SQLiteDatabase mydbnw;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.searchpage);
+		
+		Button btn2 = (Button) findViewById(R.id.button2);
+		btn2.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "hiiiiiii",
+						Toast.LENGTH_LONG).show();
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private String getDirectionsUrl(String origin,String destination) {
+		//String origin = "manipay,jaffna";
+		//String destination = "Nallur,jaffna";
+		// Building the url to the web service
+		String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="
+				+ origin
+				+ "&destinations="
+				+ destination
+				+ "&mode=driving&language=English&key=AIzaSyAdmyjZoP2uTdD9stXlJPOjD4qAWDGmN74";
+
+		return url;
+	}
+
+	/** A method to download json data from url */
+	private String downloadUrl(String strUrl) throws IOException {
+		String data = "";
+		InputStream iStream = null;
+		HttpURLConnection urlConnection = null;
+		try {
+			URL url = new URL(strUrl);
+			// Creating an http connection to communicate with url
+			urlConnection = (HttpURLConnection) url.openConnection();
+			// Connecting to url
+			urlConnection.connect();
+			// Reading data from url
+			iStream = urlConnection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					iStream));
+			StringBuffer sb = new StringBuffer();
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+			data = sb.toString();
+			br.close();
+		} catch (Exception e) {
+			// Log.d("Exception while downloading url", e.toString());
+			e.printStackTrace();
+		} finally {
+			iStream.close();
+			urlConnection.disconnect();
+		}
+		return data;
+	}
+
+	private class DownloadURLTask extends AsyncTask<String, Void, String> {
+		Object obj1, obj2, obj3, obj4, obj5;
+
+		@Override
+		protected String doInBackground(String... urls) {
+			try {
+				JSONParser parser = new JSONParser();
+				obj1 = parser.parse(urls[0]);
+				JSONArray jarray1 = (JSONArray) obj1;
+				JSONObject jobj1 = (JSONObject) jarray1.get(1);
+
+				obj2 = parser.parse((jobj1.get("rows")).toString());
+				JSONArray jarray2 = (JSONArray) obj2;
+				JSONObject jobj2 = (JSONObject) jarray2.get(0);
+
+				obj3 = parser.parse((jobj2.get("elements")).toString());
+				JSONArray jarray3 = (JSONArray) obj3;
+				JSONObject jobj3 = (JSONObject) jarray3.get(0);
+
+				obj4 = parser.parse((jobj3.get("distance")).toString());
+				String[] sarr1 = obj4.toString().split("value");
+				String[] sarr2 = sarr1[1].split("\\}");
+				String[] sarr3 = sarr2[0].split(":");
+				String[] sarr4 = sarr3[1].split(",");
+				dis = sarr4[0];
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return dis;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(getApplicationContext(), dis, Toast.LENGTH_LONG)
+					.show();
+		}
+	}
+	
+	 public void onClick(View view) {
+	  
+		 Spinner edittext1 = (Spinner) findViewById(R.id.location);
+			Spinner edittext2 = (Spinner) findViewById(R.id.b_group);
+			location = edittext1.getSelectedItem().toString();
+			bloodgroup = edittext2.getSelectedItem().toString();
+		try{
+		mydbnw = openOrCreateDatabase(ThirdActivity.DBNAME, Context.MODE_PRIVATE, null);
+		Cursor c = mydbnw.rawQuery("SELECT * from " + ThirdActivity.TABLE, null);
+		int count = c.getCount();
+		c.moveToFirst();
+		String bgrup;
+		String locatn;
+		String[] distance1=new String[count];
+		String minDis = null;
+		float defalultDis=20000;
+		for (Integer j = 0; j < count; j++) {
+			bgrup=c.getString(c.getColumnIndex("BLOOD_GROUP"));
+			if(bgrup.equals(bloodgroup)){
+				locatn=c.getString(c.getColumnIndex("LOCATION"));
+				String s = getDirectionsUrl(locatn,location);
+		DownloadURL du = new DownloadURL();
+		du.execute(new String[] { s });
+				Float f=Float.valueOf(dis)/1000;
+				distance1[j]=f.toString();
+				//minDis=distance1[j];
+				if(Integer.valueOf(distance1[j])<defalultDis){
+					minDis=distance1[j];
+				}
+				else{
+					minDis=String.valueOf(defalultDis);
+				}
+				defalultDis=Float.valueOf(distance1[j]);
+				
+			}
+			
+			c.moveToNext();
+		}
+		Toast.makeText(getApplicationContext(),minDis,Toast.LENGTH_LONG).show();
+		
+		mydbnw.close();
+		}
+		catch (Exception e) {
+			Toast.makeText(getApplicationContext(), "Error encountered.",
+					Toast.LENGTH_LONG);
+		}
+	}
+	
+
+	
+	private class DownloadURL extends AsyncTask<String, Void, String> {
+		String ur = null;
+		@Override
+		protected String doInBackground(String... urls) {
+			try {
+				ur = downloadUrl(urls[0]);
+				ur1 = "[0," + ur + "]";
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return ur1;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			DownloadURLTask dt = new DownloadURLTask();
+			dt.execute(new String[] { result });
+		}
+	}
+
+
+
+	
+		
+	}
+
